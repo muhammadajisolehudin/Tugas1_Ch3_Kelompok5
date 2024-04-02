0007b8045d05bd210d00093f1d5b995c682f2fb9
mhmadrizallll <muhammadrizal087733521@gmail.com>
2024-04-03 06:25:05 +0700

    add function validate card number

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const readLine = require('readline');
+const readLine = require("readline");
 
 const rl = readLine.createInterface({
   input: process.stdin,
@@ -6,24 +6,27 @@ const rl = readLine.createInterface({
 });
 
 const accountA = {
-  name: 'John Doe',
+  name: "John Doe",
   balance: 0,
-  pin: '123456',
-  cardNumber: '1234 5678 1234 5678',
+  pin: "123456",
+  cardNumber: "1234 5678 1234 5678",
   transactions: [],
 };
 
 const accountB = {
-  name: 'Jane Doe',
+  name: "Jane Doe",
   balance: 0,
-  pin: '654321',
-  cardNumber: '5678 1234 5678 1234',
+  pin: "654321",
+  cardNumber: "5678 1234 5678 1234",
   transactions: [],
 };
 
 const accounts = [accountA, accountB];
 
-function validateCardNumber() {}
+function validateCardNumber(cardNumber) {
+  return accounts.find((account) => account.cardNumber === cardNumber);
+}
+
 function validatePin() {}
 function checkBalance() {}
 function deposit() {}
@@ -38,18 +41,32 @@ function askQuestion(question) {
 }
 
 async function main() {
-  do {
-    console.log('Menu ATM:');
-    console.log('1. Cek Saldo');
-    console.log('2. Setor Tunai');
-    console.log('3. Riwayat Transaksi');
-    console.log('4. Keluar');
+  let currentAccount;
+  let choice;
 
-    choice = await askQuestion('Masukkan pilihan Anda: ');
+  do {
+    console.log("Menu ATM:");
+    console.log("1. Cek Saldo");
+    console.log("2. Setor Tunai");
+    console.log("3. Riwayat Transaksi");
+    console.log("4. Keluar");
+
+    choice = await askQuestion("Masukkan pilihan Anda: ");
 
     switch (parseInt(choice)) {
       case 1:
+        const cardNumber = await askQuestion("Masukkan nomor kartu: ");
+        currentAccount = validateCardNumber(cardNumber);
+        if (currentAccount) {
+          console.log("=================");
+          console.log("Selamat datang, ", currentAccount.name);
+          console.log("-----------------");
+          console.log("saldo anda saat ini", +checkBalance(currentAccount));
+        } else console.log("nomor kartu tidak ditemukan");
+        console.log("=================");
         break;
+      default:
+        console.log("Pilihan tidak valid");
     }
   } while (choice !== 4);
 }
